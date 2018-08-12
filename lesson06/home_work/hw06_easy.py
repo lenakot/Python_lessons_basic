86662f3ad33740a1100f3c6ef5c4ee2d8c6a6720
Elena Kotova <ekotova18@mail.ru>
2018-08-12 22:05:12 +0300

    Lesson 6

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,86 @@
+import math
+
+__author__ = 'Котова Елена'
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+
+def distance(a, b):
+    return math.sqrt(((b[0]-a[0]) ** 2) + ((b[1]-a[1]) ** 2))
+
+class Triangle:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.ab = distance(self.a, self.b)
+        self.bc = distance(self.b, self.c)
+        self.ca = distance(self.c, self.a)
+
+    def perimeter(self):
+        return self.ab + self.bc + self.ca
+
+    def height(self, side):
+        ab = self.ab
+        bc = self.bc
+        ca = self.ca
+        p = self.perimeter()/2
+        return (2/side) * math.sqrt(p * (p - ab) * (p - bc) * (p - ca))
+
+    # Высота из точки self.a
+    def height_a(self):
+        return self.height(self.bc)
+
+    # Высота из точки self.b
+    def height_b(self):
+        return self.height(self.ca)
+
+    # Высота из точки self.c
+    def height_c(self):
+        return self.height(self.ab)
+
+    def area(self):
+        return (self.ca/2) * self.height_b()
+
+
+tri = Triangle((1, 1), (3, 4), (5, 1))
+print('Периметр: ', tri.perimeter())
+print('Высота: ', tri.height_b())
+print('Площадь: ', tri.area())
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class IsoscelesTrapezium:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.ab = distance(self.a, self.b)
+        self.bc = distance(self.b, self.c)
+        self.cd = distance(self.c, self.d)
+        self.da = distance(self.d, self.a)
+
+    # Проверяем: трапеция равнобедренная, если её диагонали равны. Если всё ок, то вернет True.
+    def check(self):
+        return distance(self.c, self.a) == distance(self.b, self.d)
+
+    def perimeter(self):
+        return self.ab + self.bc + self.cd + self.da
+
+    def area(self):
+        return ((self.da + self.bc)/4) * math.sqrt((4 *(self.ab ** 2)) - ((self.da - self.bc) ** 2))
+
+
+trap = IsoscelesTrapezium((1, 1), (3, 5), (6, 5), (8, 1))
+print('Длина стороны ab = ', trap.ab)
+print('Длина стороны bc = ', trap.bc)
+print('Длина стороны cd = ', trap.cd)
+print('Длина стороны da = ', trap.da)
+print('Является ли трапеция равнобедренной: ', trap.check())
+print('Периметр: ', trap.perimeter())
+print('Площадь: ', trap.area())
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -1,3 +1,6 @@
+
+__author__ = 'Котова Елена'
+
 # Задание-1:
 # Реализуйте описаную ниже задачу, используя парадигмы ООП:
 # В школе есть Классы(5А, 7Б и т.д.), в которых учатся Ученики.
@@ -15,3 +18,76 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+class Teacher:
+    def __init__(self, surname, course):
+        self.surname = surname
+        self.course = course
+
+class ClassRoom:
+    def __init__(self, number, teachers):
+        self.number = number
+        self.teachers = teachers
+
+class Student:
+    def __init__(self, name, surname, mother, father, class_room):
+        self.name = name
+        self.surname = surname
+        self.mother = mother
+        self.father = father
+        self.class_room = class_room
+
+teachers = {
+    'Грибов': Teacher('Грибов', 'Математика'),
+    'Иванов': Teacher('Иванов', 'Рус.Язык'),
+    'Петров': Teacher('Петров', 'История'),
+    'Пучкин': Teacher('Пучкин', 'Биология'),
+    'Сергеева': Teacher('Сергеева', 'Литература'),
+}
+
+classes = {
+    '1А': ClassRoom('1А', ['Петров', 'Иванов', 'Сергеева']),
+    '1Б': ClassRoom('1Б', ['Сергеева', 'Пучкин']),
+    '2Б': ClassRoom('2Б', ['Петров', 'Пучкин']),
+    '3В': ClassRoom('3В', ['Иванов', 'Грибов']),
+}
+
+students = {
+   'Петр Алексеев': Student('Петр', 'Алексеев', 'Алексеева Татьяна', 'Алексеев Вадим', '1А'),
+   'Анна Алексеева': Student('Анна', 'Алексеева', 'Алексеева Татьяна', 'Алексеев Вадим', '1Б'),
+   'Василий Иванов': Student('Василий', 'Иванов', 'Плотникова Наталья', 'Иванов Иван', '2Б'),
+   'Матвей Бурин': Student('Матвей', 'Бурин', 'Бурина Евгения', 'Дурнин Пётр', '3В'),
+}
+
+# 1. Получить полный список всех классов школы
+for el in classes:
+    print(el)
+
+# 2. Получить список всех учеников в указанном классе
+#  (каждый ученик отображается в формате "Фамилия И.О.")
+class_room = '2Б'
+for el in students.values():
+    if el.class_room == class_room:
+        print(el.name, el.surname)
+
+# 3. Получить список всех предметов указанного ученика
+#  (Ученик --> Класс --> Учителя --> Предметы)
+
+fullname = 'Анна Алексеева'
+if fullname in students:
+    class_room = students[fullname].class_room
+    if class_room in classes:
+        for surname in classes[class_room].teachers:
+            if surname in teachers:
+                print(teachers[surname].course)
+
+# 4. Узнать ФИО родителей указанного ученика
+fullname = 'Анна Алексеева'
+if fullname in students:
+    print('Мать: ', students[fullname].mother)
+    print('Отец: ', students[fullname].father)
+
+# 5. Получить список всех Учителей, преподающих в указанном классе
+class_room = '2Б'
+if class_room in classes:
+    print(classes[class_room].teachers)
